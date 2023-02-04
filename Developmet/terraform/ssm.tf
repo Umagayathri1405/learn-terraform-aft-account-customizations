cc50c544e9abf8c6e0df3d0a1199609e2b63d61f
Umagayathri <u541732@bpost.be>
2023-02-04 10:57:16 +0100

    Tier-1 module added

--- Developmet/terraform/ssm.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_ssm_parameter" "foo" {
-  name  = "/acctId"
-  type  = "String"
-  value = "${data.aws_caller_identity.current.account_id}"
-}
