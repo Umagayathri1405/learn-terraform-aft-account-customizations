48432e09ff47febf6014d9c5006fc977919add87
V Umagayathri (ext) <u541732@bpost.be>
2023-10-30 07:35:44 +0100

    IAM role added in Acceptance_02

---
+++ Acceptance_02/terraform/IAM_EC2_Role.tf
@@ -0,0 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "test_role" {
+  name = "ec2_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
